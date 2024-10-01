8682baa5bbe2f51cac2ff7ed2326e6d87c4f1c68
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-01 09:51:31 +0200

    Do task 10, project 0x00. ES6 Basics

--- 0x00-ES6_basic/10-main.js
+++ 0x00-ES6_basic/10-main.js
@@ -1,7 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
-  for (const value of array) {
+  for (let value of array) {
     const index = array.indexOf(value);
-    // eslint-disable-next-line no-param-reassign
     array[index] = appendString + value;
   }
 
