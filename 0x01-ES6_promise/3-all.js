23ab057e4614adf7d6f885a36f63134752d9a699
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-02 14:08:56 +0200

    Do task 7, project 0x01. ES6 Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,34 +1,20 @@
-// import { uploadPhoto, createUser } from './utils';
-
-// function handleProfileSignup() {
-//   const photoPromise = uploadPhoto();
-//   const userPromise = createUser();
-
-//   Promise.all([photoPromise, userPromise])
-//     .then(([photoResponse, userResponse]) => {
-//       // Log firstName and lastName from userResponse
-//       if (photoResponse.status === 200) {
-//         console.log(`${userResponse.firstName} ${userResponse.lastName}`);
-//       }
-//     })
-//     .catch(() => {
-//       // Log the error in case of failure
-//       console.log('Signup system offline');
-//     });
-// }
-
-// export default handleProfileSignup();
-
 import { uploadPhoto, createUser } from './utils';
 
 function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
+  const photoPromise = uploadPhoto();
+  const userPromise = createUser();
+
+  Promise.all([photoPromise, userPromise])
     .then(([photoResponse, userResponse]) => {
-      console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      // Log firstName and lastName from userResponse
+      if (photoResponse.status === 200) {
+        console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+      }
     })
     .catch(() => {
+      // Log the error in case of failure
       console.log('Signup system offline');
     });
 }
 
-handleProfileSignup();
+export default handleProfileSignup();
