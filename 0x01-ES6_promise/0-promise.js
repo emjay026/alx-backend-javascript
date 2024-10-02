0606465ed6f89050fdfaacdc63a293b36be6b2c9
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-02 08:49:05 +0200

    re-construct project

--- 0x01-ES6_promise/0-promise.js
+++
@@ -1,6 +0,0 @@
-export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    // Simulate an asynchronous operation, for example, fetching data from an API
-    setTimeout(() => {}, 1000); // Simulate a delay of 1 second
-  });
-}
