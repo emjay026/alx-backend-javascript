86ce1f80ec80d8bfbe0be1aa5b4f68192a7c6ef2
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-02 14:13:19 +0200

    Do task 8, project 0x01. ES6 Promises

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,14 @@
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    const result = mathFunction(); // Execute the math function
+    queue.push(result); // Append the result to the queue
+  } catch (error) {
+    queue.push(error.message); // Append the error message to the queue
+  } finally {
+    queue.push('Guardrail was processed'); // Always appended to the queue
+  }
+
+  return queue; // Return the queue
+}
