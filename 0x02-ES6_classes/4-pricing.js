1075b96f6ea3f5b9cca5ee474ff0b6bcfeed60cb
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-03 21:53:19 +0200

    Do task 4, project 0x02. ES6 classes

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,58 @@
+/* eslint-disable no-underscore-dangle */
+import Currency from './3-currency';
+
+class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  // Getter and Setter for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  // Getter and Setter for currency
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  // Method to display the full price
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // Static method to convert price
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
+
+export default Pricing;
