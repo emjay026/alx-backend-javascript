1d573a9b587baf3a585128edb4b38efd17b9a6f4
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-04 00:08:13 +0200

    Do task 3, project 0x01. ES6 Promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,20 +1,13 @@
 import { uploadPhoto, createUser } from './utils';
 
-function handleProfileSignup() {
-  const photoPromise = uploadPhoto();
-  const userPromise = createUser();
+async function handleProfileSignup() {
+  const photoResponse = await uploadPhoto();
+  const userResponse = await createUser();
 
-  Promise.all([photoPromise, userPromise])
-    .then(([photoResponse, userResponse]) => {
-      // Log firstName and lastName from userResponse
-      if (photoResponse.status === 200) {
-        console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
-      }
-    })
-    .catch(() => {
-      // Log the error in case of failure
-      console.log('Signup system offline');
-    });
+  // Log firstName and lastName from userResponse
+  if (photoResponse.status === 200) {
+    console.log(`${photoResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
+  }
 }
 
-export default handleProfileSignup();
+export default handleProfileSignup;
