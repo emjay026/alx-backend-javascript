ca3b0cdbf97f62d8f39b0b73834646e9bdd1c4c3
emjay026 <mojalefa.emjay01@gmail.com>
2024-10-08 13:13:52 +0200

    Do task 8, project 0x03. ES6 data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,24 @@
 function cleanSet(inputSet, startString) {
-  // if (!startString) { // Return an empty string if startString is empty
-  //   return '';
-  // }
+  // Check if inputSet is an array
+  if (!Array.isArray(inputSet)) {
+    throw new TypeError('inputSet must be an array');
+  }
+
+  // Check if startString is a string
+  if (typeof startString !== 'string') {
+    throw new TypeError('startString must be a string');
+  }
+
+  // Return an empty string if startString is empty
+  if (!startString) {
+    return '';
+  }
 
   const result = [];
+
   inputSet.forEach((value) => {
-    if (value.startsWith(startString)) {
+    // Check if value is a string
+    if (typeof value === 'string' && value.startsWith(startString)) {
       result.push(value.slice(startString.length)); // Append the rest of the string
     }
   });
