d4237766f9455c66495138b1bb02ced4f656df09
Mojalefa <mojalefa.emjay01@gmail.com>
2024-11-27 00:16:05 +0200

    Do task 4, project 0x05. NodeJS Basics

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,16 +3,16 @@ const http = require('http');
 
 // Create a small HTTP server
 const app = http.createServer((req, res) => {
-    // Set the response HTTP header with HTTP status and content type
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+  // Set the response HTTP header with HTTP status and content type
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
 
-    // Send the response body as 'Hello Holberton School!'
-    res.end('Hello Holberton School!');
+  // Send the response body as 'Hello Holberton School!'
+  res.end('Hello Holberton School!');
 });
 
 // Make the server listen on port 1245
 app.listen(1245, () => {
-    console.log('...');
+  console.log('...');
 });
 
 // Export the app to make it available for other modules
