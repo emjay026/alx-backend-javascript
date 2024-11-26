3e5043a6aba543977584c51a6b1c7179b1d96a0b
Mojalefa <mojalefa.emjay01@gmail.com>
2024-11-26 22:28:10 +0200

    Do task 0, project 0x05. NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,8 @@
+// Define a function named displayMessage that takes a single argument 'message'
+function displayMessage(message) {
+  // Print the message to the standard output using console.log
+  console.log(message);
+}
+
+// Export the displayMessage function so that it can be used in other scripts
+module.exports = displayMessage;
